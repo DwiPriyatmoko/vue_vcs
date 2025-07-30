56bf0de12d19c25d8b327cf5d9e219df4ce3805a
dwiPriyatmoko <dwipriyatmoko27@gmail.com>
2025-07-30 12:04:05 +0700

    initial commit: vue vcs

---
+++ src/components/MainButton.vue
@@ -0,0 +1,18 @@
+<script setup>
+  const { title, disabled } = defineProps({
+    title: {
+      type: String,
+      default: 'Subscribe',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  });
+</script>
+
+<template>
+  <button :disabled>{{ title }}</button>
+</template>
+
+<style scoped></style>
