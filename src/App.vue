096d2c51ca473e64c41d5d5942280f67d54909f9
dwiPriyatmoko <dwipriyatmoko27@gmail.com>
2025-07-30 23:22:33 +0700

    feat: 13. Event Handling

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <script setup>
 import { ref } from 'vue';
-import MainButton from './components/MainButton.vue';
+// import MainButton from './components/MainButton.vue';
 
-const disabled = ref(false);
-const toDisable = () => {
-  disabled.value = !disabled.value;
+// const disabled = ref(false);
+// const toDisable = () => {
+//   disabled.value = !disabled.value;
+// };
+const count = ref(0);
+
+const increment = (counter) => {
+  count.value = count.value + counter;
 };
 </script>
 
 <template>
-  <MainButton @click="toDisable" :disabled />
+  <!-- <MainButton @click="toDisable" :disabled /> -->
+  <button @click="(event) => console.log(event)" @mouseover="increment(10)">
+    {{ count }}
+  </button>
+
+  <input @keyup.enter="() => console.log('Enter pressed')" placeholder="Press Enter" />
 </template>
 
 <style scoped></style>
